5d8a451a89d1d349945c5945c3e8690065124419
4087 <falconrobotics@bfhsla.org>
2020-05-21 14:24:22 -0500

    Competition changes

--- src/main/java/frc/robot/commands/launch.java
+++ src/main/java/frc/robot/commands/launch.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import com.revrobotics.ControlType;
 
-import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
